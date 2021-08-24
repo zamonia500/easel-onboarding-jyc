f21416b3a49453fb01db1b3fa2a753cde9a13cbe
YoungCheol Jang <youngcheol.jang@makinarocks.ai>
2021-08-24 14:09:27 +0900

    [ch-8119] echo server initial drop

---
+++ echo_server/app/routers/echo.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from fastapi import APIRouter, Request
+
+router = APIRouter()
+
+
+@router.get("/echo/", tags=["users"])
+async def echo_query_parameter(request: Request):
+
+    result = dict()
+
+    for key in request.query_params.keys():
+        param_list: List = request.query_params.getlist(key)
+        result[key] = param_list if len(param_list) > 1 else param_list[0]
+
+    return result
